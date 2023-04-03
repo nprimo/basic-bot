1178d43f6b1c9b4c34d6f7b06049ac8cd2f8812f
nprimo <primo.niccolo@gmail.com>
2023-04-03 20:02:24 +0200

    feat(bot): update commands function
    - make commands function more flexible

--- bot.py
+++ bot.py
@@ -9,10 +9,10 @@ def hello(message: discord.Message, av: list[str]) -> str:
 def commands(key: str, message: discord.Message, av: list[str]) -> str:
     commands = {
         # help -> show all the commands options
-        '$hello': hello,
+        'hello': hello,
         # additional commands
     }
-    command_to_exec = commands.get(key)
+    command_to_exec = commands.get(key[1:])
     if command_to_exec:
         return command_to_exec(message, av)
     return 'Unknown command'
